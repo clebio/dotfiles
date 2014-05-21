e648156dc4c61cf8d2a17a0eb6beedfd39950243
Caleb Hyde <caleb.hyde@gmail.com>
2014-05-20 19:51:47 -0500

    Update battery-notify; add suspend.sh

--- battery-notify.sh
+++ battery-notify.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-BATTERY_CHARGE=$(acpi -b | sed -n 's/.*Discharging, \([0-9]\)%.*/\1/p')
+BATTERY_CHARGE=$(acpi -b | sed -n 's/.Discharging, \([0-9]\)%.*/\1/p')
 
 if [ -n "${BATTERY_CHARGE}" ]
 then
     if [ "${BATTERY_CHARGE}" -le "5" ]
     then
-        zenity --warning --width=500 --height=300 --text="Battery critically low!"
+        /usr/bin/zenity --warning --width=500 --height=300 --text="Battery critically low!"
     #    poweroff
     else 
-        notify-send "Battery critically low: ${BATTERY_CHARGE}"
+        /usr/bin/notify-send "Battery low: ${BATTERY_CHARGE}"
     fi
 fi
-
 
