766dde02210f1502be6be9ebb8bb24c47545b703
Caleb Hyde <caleb.hyde@gmail.com>
2014-06-29 08:50:06 -0500

    Remove dotfiles that we're going to symlink

--- link_dots.sh
+++ link_dots.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-find ./ -name '_*' -exec bash -c 'ln -s ${HOME}/dotfiles/${1/\.\//} ${HOME}/${1/\.\/\_/\.}' -- {} \;
+export DOTFILE_DIR=dotfiles
+
+# Rename existing dot files that we track from ${HOME} to a '.bak' copy
+find ./ -name '_*' \
+    -exec bash -c 'find ${HOME}/ -maxdepth 1 -name "${1/\.\/\_/.}" \
+    | xargs -I'{}' mv {} {}.bak' -- {} \;
+
+# Symlink underscored files in DOTFILE_DIR to HOME/.<filename>
+find ./ -name '_*' \
+    -exec bash -c 'ln -s ${HOME}/${DOTFILE_DIR}/${1/\.\//} ${HOME}/${1/\.\/\_/\.}' -- {} \;
+
+unset DOTFILE_DIR
