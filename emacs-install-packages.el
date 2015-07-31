a1cad36ffb045c9a09d435ebcbd9abc713e6d2f4
Caleb Hyde <chyde@alightanalytics.com>
2015-07-31 15:23:10 -0500

    Add Vagrant and Ansible packages

---
+++ emacs-install-packages.el
@@ -0,0 +1,24 @@
+(require 'cl)
+
+(defvar my-packages
+  '(ack-and-a-half auctex clojure-mode coffee-mode deft expand-region
+                   gist groovy-mode haml-mode haskell-mode inf-ruby
+                   magit magithub markdown-mode paredit projectile python
+                   sass-mode rainbow-mode scss-mode solarized-theme
+                   volatile-highlights yaml-mode yari zenburn-theme
+		   ansible ansible-doc vagrant vagrant-tramp
+		   )
+  "A list of packages to ensure are installed at launch.")
+
+(defun my-packages-installed-p ()
+  (loop for p in my-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (my-packages-installed-p)
+  ;; check for new packages (package versions)
+  (package-refresh-contents)
+  ;; install the missing packages
+  (dolist (p my-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
